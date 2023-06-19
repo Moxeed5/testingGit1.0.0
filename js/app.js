bd9f552b29b1ede434c3907af17b8ec159db1be2
Max Morgan <maxmmorgan93@gmail.com>
2023-06-19 13:44:49 -0500

    scroll animation css 1st commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+(function() {
+    document.addEventListener('DOMContentLoaded', (event) => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+            });
+        });
+
+        document.querySelectorAll('.hidden').forEach(section => {
+            observer.observe(section);
+        });
+    });
+})();
